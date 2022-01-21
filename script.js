520b97344fa50018f6395a15352ad337923a74c7
Ayman Mohamed <aymantaha632@gmail.com>
2022-01-20 19:53:13 -0500

    implementing modal
    - adding modal on btn clicks
    - closing modal on x click, click outside, esc press

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 'use strict';
+// constants
+
+const showModalBtns = document.querySelectorAll('.btn--show-modal');
+const closeModalBtn = document.querySelector('.btn--close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+//functions
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+//Events
+
+//modal controls
+
+showModalBtns.forEach(function (btn) {
+  btn.addEventListener('click', openModal);
+});
+
+closeModalBtn.addEventListener('click', closeModal);
+document.addEventListener('keyup', function (e) {
+  if (e.key === 'Escape') {
+    closeModal();
+  }
+});
+
+overlay.addEventListener('click', function (e) {
+  if (!modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
